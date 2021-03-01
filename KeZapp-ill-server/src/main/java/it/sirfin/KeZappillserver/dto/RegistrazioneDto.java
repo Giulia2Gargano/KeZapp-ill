b2cdc59b25dc90ee957c2ea03a2858f88d0d1cf4
MatteoPalombo99 <78491909+MatteoPalombo99@users.noreply.github.com>
2021-03-01 12:24:55 +0100

    Update Dto

--- KeZapp-ill-server/src/main/java/it/sirfin/KeZappillserver/dto/RegistrazioneDto.java
+++ KeZapp-ill-server/src/main/java/it/sirfin/KeZappillserver/dto/RegistrazioneDto.java
@@ -5,10 +5,56 @@
  */
 package it.sirfin.KeZappillserver.dto;
 
+import it.sirfin.KeZappillserver.model.Chat;
+import it.sirfin.KeZappillserver.model.Messaggio;
+import java.util.List;
+
 /**
  *
  * @author Palo
  */
 public class RegistrazioneDto {
-    
+
+    private List<Chat> contatti;
+    private List<Messaggio> messaggi;
+    private String sessione;
+
+    public RegistrazioneDto() {
+    }
+
+    public RegistrazioneDto(List<Chat> contatti, List<Messaggio> messaggi, String sessione) {
+        this.contatti = contatti;
+        this.messaggi = messaggi;
+        this.sessione = sessione;
+    }
+
+    public List<Chat> getContatti() {
+        return contatti;
+    }
+
+    public void setContatti(List<Chat> contatti) {
+        this.contatti = contatti;
+    }
+
+    public List<Messaggio> getMessaggi() {
+        return messaggi;
+    }
+
+    public void setMessaggi(List<Messaggio> messaggi) {
+        this.messaggi = messaggi;
+    }
+
+    public String getSessione() {
+        return sessione;
+    }
+
+    public void setSessione(String sessione) {
+        this.sessione = sessione;
+    }
+
+    @Override
+    public String toString() {
+        return "RegistrazioneDto{" + "contatti=" + contatti + ", messaggi=" + messaggi + ", sessione=" + sessione + '}';
+    }
+
 }
