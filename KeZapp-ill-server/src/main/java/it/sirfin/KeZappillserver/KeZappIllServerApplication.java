0dc540ec1ef979ba32bef7ceca74bcbbc5011608
Giulia2Gargano <giulia2gargano@gmail.com>
2021-03-01 11:48:31 +0100

    first commit

---
+++ KeZapp-ill-server/src/main/java/it/sirfin/KeZappillserver/KeZappIllServerApplication.java
@@ -0,0 +1,13 @@
+package it.sirfin.KeZappillserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeZappIllServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeZappIllServerApplication.class, args);
+	}
+
+}
---
+++ KeZapp-ill-server/src/test/java/it/sirfin/KeZappillserver/KeZappIllServerApplicationTests.java
@@ -0,0 +1,13 @@
+package it.sirfin.KeZappillserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeZappIllServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
