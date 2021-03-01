5c0264b98c24ec119c792032d7d2379171d3a731
MatteoPalombo99 <78491909+MatteoPalombo99@users.noreply.github.com>
2021-03-01 14:13:46 +0100

    Update Controller

--- KeZapp-ill-server/src/main/java/it/sirfin/KeZappillserver/controller/KeZappController.java
+++ KeZapp-ill-server/src/main/java/it/sirfin/KeZappillserver/controller/KeZappController.java
@@ -5,9 +5,14 @@
  */
 package it.sirfin.KeZappillserver.controller;
 
+import it.sirfin.KeZappillserver.dto.InviaMessaggioDto;
+import it.sirfin.KeZappillserver.dto.RegistrazioneDto;
+import it.sirfin.KeZappillserver.dto.RichiediMessaggioDto;
+import it.sirfin.KeZappillserver.dto.RichiediRegistrazioneDto;
 import it.sirfin.KeZappillserver.service.KeZappService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin("*")
@@ -17,4 +22,23 @@ public class KeZappController {
     @Autowired
     KeZappService keZappService;
 
+    @RequestMapping("/registrazione")
+    public RegistrazioneDto registrazione(RichiediRegistrazioneDto reqDto) {
+        return registrazione(reqDto);
+    }
+
+    @RequestMapping("/invia-tutti")
+    public RegistrazioneDto inviaATutti(InviaMessaggioDto reqDto) {
+        return inviaATutti(reqDto);
+    }
+
+    @RequestMapping("/invia-uno")
+    public RegistrazioneDto inviaAUno(InviaMessaggioDto reqDto) {
+        return inviaAUno(reqDto);
+    }
+
+    @RequestMapping("/aggiorna")
+    public RegistrazioneDto aggiorna(RichiediMessaggioDto reqDto) {
+        return aggiorna(reqDto);
+    }
 }
