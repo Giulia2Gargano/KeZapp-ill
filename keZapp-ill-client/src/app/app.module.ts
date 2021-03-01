056e97e4aa63f14962f0b4c4039a10523dffdcf3
MatteoPalombo99 <78491909+MatteoPalombo99@users.noreply.github.com>
2021-03-01 12:51:41 +0100

    Update Modulo http

--- keZapp-ill-client/src/app/app.module.ts
+++ keZapp-ill-client/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
