fd965e26c610654adb2542c24551a0b0c6754cee
MatteoPalombo99 <78491909+MatteoPalombo99@users.noreply.github.com>
2021-03-01 16:47:46 +0100

    Update KezappServiceImpl.java

--- KeZapp-ill-server/src/main/java/it/sirfin/KeZappillserver/service/impl/KezappServiceImpl.java
+++ KeZapp-ill-server/src/main/java/it/sirfin/KeZappillserver/service/impl/KezappServiceImpl.java
@@ -9,6 +9,7 @@ import it.sirfin.KeZappillserver.dto.InviaMessaggioDto;
 import it.sirfin.KeZappillserver.dto.RegistrazioneDto;
 import it.sirfin.KeZappillserver.dto.RichiediMessaggioDto;
 import it.sirfin.KeZappillserver.dto.RichiediRegistrazioneDto;
+import it.sirfin.KeZappillserver.model.Chat;
 import it.sirfin.KeZappillserver.repository.KeZappRepositoryChat;
 import it.sirfin.KeZappillserver.service.KeZappService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,7 +23,7 @@ public class KeZappServiceImpl implements KeZappService {
 
     @Override
     public RegistrazioneDto registrazione(RichiediRegistrazioneDto reqregDto) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        keZappRepositoryChat.save(reqregDto.getNickname());
     }
 
     @Override
