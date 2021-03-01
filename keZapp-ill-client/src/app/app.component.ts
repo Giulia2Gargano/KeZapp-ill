87519e94e7e3534294b4a0894d71d8ce8f365116
MatteoPalombo99 <78491909+MatteoPalombo99@users.noreply.github.com>
2021-03-01 17:28:10 +0100

    Update client

--- keZapp-ill-client/src/app/app.component.ts
+++ keZapp-ill-client/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { Chat } from './chat';
+import { InviaMessaggioDto } from './invia-messaggio-dto';
 import { RegistrazioneDto } from './registrazione-dto';
 import { RichiediRegistrazioneDto } from './richiedi-registrazione-dto';
 
@@ -12,11 +13,11 @@ import { RichiediRegistrazioneDto } from './richiedi-registrazione-dto';
 export class AppComponent {
   contatto = new Chat();
   contatti: Chat[] = [];
-
+  messaggio = "";
   constructor(private http: HttpClient) { }
 
   registrazione() {
-    
+
     let req = new RichiediRegistrazioneDto();
     req.nickname = this.contatto.nickname;
     let oss = this.http.post<RegistrazioneDto>("http://localhost:8080/registrazione", req);
@@ -24,4 +25,24 @@ export class AppComponent {
     this.contatto = new Chat();
 
   }
+
+  inviaATutti() {
+    let req = new InviaMessaggioDto();
+    let oss = this.http.post<RegistrazioneDto>("http://localhost:8080/invia-tutti", req);
+    oss.subscribe();
+  }
+
+  inviaAUno() {
+    let req = new InviaMessaggioDto();
+    //req.destinatario = this.contatto.nickname;
+    let oss = this.http.post<RegistrazioneDto>("http://localhost:8080/invia-uno", req);
+    oss.subscribe();
+  }
+
+  aggiorna() {
+    let oss = this.http.get<RegistrazioneDto>("http://localhost:8080/aggiorna");
+    oss.subscribe(r => this.contatti = r.contatti);
+  }
+
+
 }
